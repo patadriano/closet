9176ff9afde2ea70a1a925bfad8e1af0edc23c2b
patadriano <patriciamae1602@gmail.com>
2025-02-14 16:50:48 +0800

    test partial postback

--- Closeted/Program.cs
+++ Closeted/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Test}/{action=Test}/{id?}");
 
 app.Run();
